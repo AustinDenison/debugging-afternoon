1c5d2f6dc08f62f97aab2f2ec17ced0b79f14fe0
AustinDenison <austindenison1998@gmail.com>
2019-06-25 15:51:54 -0600

    done with black diamond

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; 
 import axios from "axios";
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
@@ -12,7 +12,12 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.addToCart = this.addToCart.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
+    this.navigate = this.navigate.bind(this)
   }
+
   componentDidMount() {
     axios
       .get("https://practiceapi.devmountain.com/products/")
@@ -22,9 +27,9 @@ class App extends Component {
         });
       });
   }
-  addToCart(item) {
+  addToCart(element) {
     this.setState({
-      cart: [...this.state.cart, item]
+      cart: [...this.state.cart, element]
     });
   }
   removeFromCart(index) {
@@ -36,9 +41,9 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({showCart: true});
     } else {
-      this.state.showCart = false;
+      this.setState({showCart: false});
     }
   }
   render() {
